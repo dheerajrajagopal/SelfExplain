bd149edb6f518ea29dd2f2966f15738957a9c659
dheeraj <dheeraj.gopal@gmail.com>
2021-03-03 13:22:44 -0500

    [README] update README, scripts

--- scripts/run_self_explain.sh
+++ scripts/run_self_explain.sh
@@ -3,5 +3,5 @@ export TOKENIZERS_PARALLELISM=false
 python model/run.py --dataset_basedir data/ \
                          --lr 2e-5  --max_epochs 20 \
                          --gpus 2 \
-                         --concept_store data/SST-2-XLNet/concept_store.pt \
+                         --concept_store data/concept_store.pt \
                          --accelerator ddp
