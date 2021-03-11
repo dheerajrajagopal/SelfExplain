be7fba3d4916f4579b04e2b13323c1506911b08a
dheeraj <dheeraj.gopal@gmail.com>
2021-03-11 11:28:00 -0500

    [INFERENCE] updated inference code, and model name to main

--- scripts/run_self_explain.sh
+++ scripts/run_self_explain.sh
@@ -4,6 +4,4 @@ python model/run.py --dataset_basedir data/ \
                          --lr 2e-5  --max_epochs 5 \
                          --gpus 2 \
                          --concept_store data/concept_store.pt \
-                         --accelerator ddp \
-                         --default_root_dir  "seed_18_k=10_multihead" \
-                         --num_train_samples
+                         --accelerator ddp
