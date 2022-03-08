395f7dcbde6c8f436d40d0d68cac867e92c8eae5
Xuweiyi Chen <62165079+XuweiyiChen@users.noreply.github.com>
2022-03-08 15:42:31 -0800

    Update run_self_explain.sh

--- scripts/run_self_explain.sh
+++ scripts/run_self_explain.sh
@@ -2,6 +2,6 @@
 export TOKENIZERS_PARALLELISM=false
 python model/run.py --dataset_basedir data/SST-2-XLNet \
                          --lr 2e-5  --max_epochs 5 \
-                         --gpus 2 \
+                         --gpus 1 \
                          --concept_store data/SST-2-XLNet/concept_store.pt \
                          --accelerator ddp
